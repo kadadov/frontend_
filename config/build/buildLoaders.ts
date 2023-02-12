8fd03fc8480ad2c4e15c573019e89a95affe7a2e
Kadadov Sasha <kadadov.sasha@gmail.com>
2023-02-12 22:59:46 +0300

    Конфигурация проекта
    - добавлен Babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,16 +1,27 @@
-import { type RuleSetRule } from "webpack"
+import { type RuleSetRule, } from "webpack"
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
 
-import { type BuildOptions } from "./types/config"
-import { checkStyleModule } from "./lib/checkStyleModule"
+import { type BuildOptions, } from "./types/config"
+import { checkStyleModule, } from "./lib/checkStyleModule"
 
-export const buildLoaders = (options: BuildOptions): RuleSetRule[] => {
-  const { isDev } = options
+export const buildLoaders = (options: BuildOptions,): RuleSetRule[] => {
+  const { isDev, } = options
 
   const typescriptLoader = {
-    test: /\.tsx?$/,
+    test: /\.(ts|tsx)?$/,
     use: "ts-loader",
-    exclude: /node_modules/
+    exclude: /node_modules/,
+  }
+
+  const babelLoader = {
+    test: /\.(ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env", ],
+      },
+    },
   }
 
   const styleLoader = {
@@ -22,30 +33,36 @@ export const buildLoaders = (options: BuildOptions): RuleSetRule[] => {
         options: {
           modules: {
             auto: checkStyleModule,
-            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]"
-          }
-        }
+            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+          },
+        },
       },
-      "sass-loader"
-    ]
+      "sass-loader",
+    ],
   }
 
   const svgLoader = {
     test: /\.svg$/i,
     issuer: /\.[jt]sx?$/,
-    use: ["@svgr/webpack"]
+    use: ["@svgr/webpack", ],
   }
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
       {
-        loader: "file-loader"
-      }
-    ]
+        loader: "file-loader",
+      },
+    ],
   }
 
-  const plugins = [typescriptLoader, styleLoader, svgLoader, fileLoader]
+  const plugins = [
+    babelLoader,
+    typescriptLoader,
+    styleLoader,
+    svgLoader,
+    fileLoader,
+  ]
 
   return plugins
 }
